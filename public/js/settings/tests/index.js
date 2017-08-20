d5a2c90ac121d7c7e30980c1e5f74483cda3bf44
Andrea Zucchini <azga84@gmail.com>
2017-08-20 19:23:22 +0200

    remove fonts from cache

--- public/js/settings/tests/index.js
+++ public/js/settings/tests/index.js
@@ -183,9 +183,7 @@ export default {
         '/',
         '/js/main.js',
         '/css/main.css',
-        '/imgs/icon.png',
-        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
-        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+        '/imgs/icon.png'
       ].map(url => new URL(url, location).href)
 
       return caches.has('twittar-static-v1').then(has => {
--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -4,9 +4,7 @@ self.addEventListener('install', function (event) {
     '/',
     '/js/main.js',
     'css/main.css',
-    'imgs/icon.png',
-    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
-    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+    'imgs/icon.png'
   ]
   // twittar-static-v1
   event.waitUntil(
