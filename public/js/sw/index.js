eb2cc5f412bbd2f4f10814a88dc923df29789c90
Andrea Zucchini <azga84@gmail.com>
2017-08-20 16:02:08 +0200

    Responds with GIF when 404 error occurs

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,9 +1,14 @@
 self.addEventListener('fetch', (event) => {
-  if (event.request.url.endsWith('.jpg')) {
-    // this tells the browser that we're going to handle this request ourselves
-    // Event or respondWith takes a response object or a promise that resolves with a response.
-    event.respondWith(
-      fetch('/imgs/dr-evil.gif')
-    )
-  }
+  // Event or respondWith takes a response object or a promise that resolves with a response.
+  // this tells the browser that we're going to handle this request ourselves
+  event.respondWith(
+    fetch(event.request).then(function (response) {
+      if (response.status === 404) {
+        return fetch('/imgs/dr-evil.gif')
+      }
+      return response
+    }).catch(function () {
+      return new Response('Uh oh, that totally failed!!')
+    })
+  )
 })
