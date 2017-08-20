7c123297979593bf89726ae936a2e42fd0a0dac0
Andrea Zucchini <azga84@gmail.com>
2017-08-20 15:41:49 +0200

    Intercept requests for jpg images

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,3 +1,9 @@
 self.addEventListener('fetch', (event) => {
-  console.log(event.request)
+  if (event.request.url.endsWith('.jpg')) {
+    // this tells the browser that we're going to handle this request ourselves
+    // Event or respondWith takes a response object or a promise that resolves with a response.
+    event.respondWith(
+      fetch('/imgs/dr-evil.gif')
+    )
+  }
 })
