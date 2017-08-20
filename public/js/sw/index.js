4ac2e3af95ed63bc03558fb3bc958381a24e84b3
Andrea Zucchini <azga84@gmail.com>
2017-08-20 19:17:40 +0200

    cached resources

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -18,13 +18,8 @@ self.addEventListener('fetch', (event) => {
   // Event or respondWith takes a response object or a promise that resolves with a response.
   // this tells the browser that we're going to handle this request ourselves
   event.respondWith(
-    fetch(event.request).then(function (response) {
-      if (response.status === 404) {
-        return fetch('/imgs/dr-evil.gif')
-      }
-      return response
-    }).catch(function () {
-      return new Response('Uh oh, that totally failed!!')
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request)
     })
   )
 })
